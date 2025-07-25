9f81cdf674bcf3b9147237eccaebcb723b1ad70e
Denys <denisshevchenko109@gmail.com>
2025-07-25 22:33:51 +0200

    Move vite to dependencies and create ultra-simple build command

--- vite.config.prod.ts
+++ vite.config.prod.ts
@@ -1,17 +1,13 @@
 import { defineConfig } from 'vite';
-import { fileURLToPath } from 'url';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
-import path from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react(), svgr()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': resolve(__dirname, 'src'),
     },
   },
   build: {
